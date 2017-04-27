acfa7b921780b4aa56b88641402d72342ddb4f95
Richard Armuelles <lexanderarmuelles@riseup.net>
2017-04-27 03:03:12 -0500

    update vocaloids principales

---
+++ images/kaito/Readme.txt
@@ -0,0 +1,70 @@
+○MMD用モデルデータ　hzeoV3kaito200.pmx、hzeoV3kaito200_under.pmx(アンダーシャツver）、hzeo式hzeoV3kaito200_under改.pmx（アンダーシャツ改ver）、200maffonly.pmx
+
+★hzeoV3kaito202B.pmxとhzeoV3kaito202_underB.pmxを追加しました。
+★その他各箇所微調整しました。（2014/08/05）
+
+
+■内容物
+
+hzeoV3kaito202.pmx（V3カイト）
+hzeoV3kaito202_under.pmx(V3kaitoアンダーシャツver）
+hzeoV3kaito201_under改.pmx（アンダーシャツ改ver）
+200maffonly.pmx
+
+hzeoV3kaito202B.pmx（V3カイト）
+hzeoV3kaito202_underB.pmx(V3kaitoアンダーシャツver）
+
+
+（モーフ（涙　及び　赤面）の材質をVPVPwikiより　hair_a.spa、bn01.spa～bn05.spaをスフィアマッP様のサイトよりお借りしました。ありがとうございます。）
+
+
+　この度は　hzeo式kaitoV3ver2.0、hzeo式kaitoV3ver2.0under、hzeo式kaitoV3ver2.0under改、（アンダーシャツ改ver）、200maffonly.pmxをDLして頂きありがとうございます。
+
+
+
+　1.12版からの変更点
+　※身長、頭身、顔、髪型　が微妙に変わりました。
+　※モーフ「瞳左赤」「瞳右赤」「瞳左青」「瞳右青」「瞳AL」「牙」「牙AL」「ぺろっ」」「上歯調節」「口角開く」「ω」「下歯消し」「舌位置調節」「レンズ黒」「瞳スフィアoff」の追加。
+　※ヘッドホンの変更
+　※口内入れ替え
+　※耳変更
+　※眼鏡ボーンの追加
+　
+　
+　★このモデルは改造再配布を可能とします。
+　  モデル自体のパラメーターの改造　又　他のモデルとの首の差しかえによる利用などは構いませんが　どこかに制作が私であることを明記して頂ければ嬉しいです。
+
+　★他モデルとの合体による改造は　他モデルのReadmeをよくご参照の上　個々のモデルの制作者様の規則に従っておこなっていただくよう心よりお願いいたします。
+
+　★モーフ　瞳AL 牙AL　はAutoLuminousを使ったときのみ有効なモーフです。瞳、牙が光ります。
+
+　★モーフ　あ　の口の形は作者の好みで笑顔で歌うようになっています。正しい　あ　の形は　あ２　なのでそちらを使用したい場合は　PMDEditorのモーフタブで　あ　をあ２にし　あ２を　あ　にリネイムしてから　モデルを別名保存しそちらのモデルでリップモーションを読み込むと正しい　あ　の口の形で歌うようになります。
+
+
+
+■ご注意
+　・このモデルを用いて弱者（女性、子ども、老人etc）に対する　暴力、殺人、犯罪行為などに使用することを禁止します。
+
+　・モデル（キャラクター）の利用につきましてはクリプトン・フューチャー・メディア株式会社様の
+キャラクター利用のガイドライン（http://piapro.jp/license/character_guideline）をお読みください。
+
+・データの利用は自己責任でお願いします。データを利用したことにより、損害・不利益・事故等が発生した場合でも、一切の責任を負いません。
+
+
+
+■製作に使用させて頂いたソフト
+
+MikuMikuDance	作者：樋口Ｍ様（http://www.geocities.jp/higuchuu4/index.htm）
+PMDEditor　　　作者：極北P様　（VPVPwikiより）
+メタセコイア　　作者：O.Mizno様（http://www.metaseq.net/metaseq/index.html）
+
+
+■参考資料
+
+３DCG日和（かこみき様）
+VPVPwiki　http://www6.atwiki.jp/vpvpwiki/
+
+■モデル監査　マグ様
+
+
+制作者　hzeo（hzeo000@mail.goo.ne.jp）
--- scripts/vocaloid_changer.js
+++ scripts/vocaloid_changer.js
@@ -9,6 +9,7 @@ AFRAME.registerComponent('vocaloid', {
       var music;
       var model;
       var model2;
+      var model3;
 
       if(vocaloid_changer.value == 'miku'){
         $('a-entity[mmd]').remove();
@@ -19,6 +20,7 @@ AFRAME.registerComponent('vocaloid', {
 
         model = document.createElement("a-entity");
         model.setAttribute('mmd-model', 'model:/images/miku/miku_v3.pmx;vmd:/images/miku/world_is_mine.vmd;');
+        model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
       }
@@ -32,11 +34,65 @@ AFRAME.registerComponent('vocaloid', {
 
         model = document.createElement("a-entity");
         model.setAttribute('mmd-model', 'model:/images/rin/rin_2.pmx;vmd:images/rin/Rin_Migikata_no_chou.vmd;');
+        model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
         model2 = document.createElement("a-entity");
         model2.setAttribute('mmd-model', 'model:/images/len/len_2.pmx;vmd:images/len/Len_Migikata_no_chou.vmd;');
+        model2.setAttribute('shadow', 'cast:true');
         music.append(model2);
+
+      }
+
+      else if (vocaloid_changer.value == 'luka') {
+        $('a-entity[mmd]').remove();
+
+        music = document.createElement("a-entity");
+        music.setAttribute('mmd', 'audio:images/vocaloid_songs/night_fever.mp3; audioDelayTime:5.333333333333333; afterglow:2.0;');
+        self.el.append(music);
+
+        model = document.createElement("a-entity");
+        model.setAttribute('mmd-model', 'model:/images/luka/luka1.pmd;vmd:images/luka/night_fever.vmd;');
+        model.setAttribute('shadow', 'cast:true');
+        music.append(model);
+
+      }
+
+      else if (vocaloid_changer.value == 'meiko') {
+        $('a-entity[mmd]').remove();
+
+        music = document.createElement("a-entity");
+        music.setAttribute('mmd', 'audio:images/vocaloid_songs/nostalgic.mp3; audioDelayTime:5.333333333333333; afterglow:2.0;');
+        self.el.append(music);
+
+        model = document.createElement("a-entity");
+        model.setAttribute('mmd-model', 'model:/images/meiko/meiko.pmx;vmd:images/meiko/nostalgic.vmd;');
+        model.setAttribute('shadow', 'cast:true');
+        music.append(model);
+
+      }
+
+      else if (vocaloid_changer.value == 'kaito_miku_len') {
+        $('a-entity[mmd]').remove();
+
+        music = document.createElement("a-entity");
+        music.setAttribute('mmd', 'audio:images/vocaloid_songs/wave.mp3; audioDelayTime:5.333333333333333; afterglow:2.0;');
+        self.el.append(music);
+
+        model = document.createElement("a-entity");
+        model.setAttribute('mmd-model', 'model:/images/kaito/hzeoV3kaito203.pmx; vmd:images/kaito/Tda_R.vmd;');
+        model.setAttribute('shadow', 'cast:true');
+        music.append(model);
+
+        model2 = document.createElement("a-entity");
+        model2.setAttribute('mmd-model', 'model:/images/len/len_2.pmx; vmd:images/kaito/Tda_C.vmd;');
+        model2.setAttribute('shadow', 'cast:true');
+        music.append(model2);
+
+        model3 = document.createElement("a-entity");
+        model3.setAttribute('mmd-model', 'model:/images/miku/miku_v3.pmx; vmd:images/kaito/Tda_L.vmd;');
+        model3.setAttribute('shadow', 'cast:true');
+        music.append(model3);
 
       }
 
