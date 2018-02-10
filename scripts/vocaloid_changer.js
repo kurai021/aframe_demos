9e1b92d734a4abbd56dbac8c3573d1d27ffb875a
Richard Armuelles <alexanderarmuelles@riseup.net>
2018-02-09 19:45:30 -0500

    texture vocaloid fixed

--- scripts/vocaloid_changer.js
+++ scripts/vocaloid_changer.js
@@ -19,7 +19,7 @@ AFRAME.registerComponent('vocaloid', {
         self.el.append(music);
 
         model = document.createElement("a-entity");
-        model.setAttribute('mmd-model', 'model:/images/alt_miku/alt_miku.pmx;vmd:/images/miku/world_is_mine.vmd;');
+        model.setAttribute('mmd-model', 'model:images/alt_miku/alt_miku.pmx;vmd:/images/miku/world_is_mine.vmd;');
         model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
@@ -33,12 +33,12 @@ AFRAME.registerComponent('vocaloid', {
         self.el.append(music);
 
         model = document.createElement("a-entity");
-        model.setAttribute('mmd-model', 'model:/images/rin/rin_2.pmd;vmd:images/rin/Rin_Migikata_no_chou.vmd;');
+        model.setAttribute('mmd-model', 'model:images/rin/rin_2.pmd;vmd:images/rin/Rin_Migikata_no_chou.vmd;');
         model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
         model2 = document.createElement("a-entity");
-        model2.setAttribute('mmd-model', 'model:/images/len/len_2.pmx;vmd:images/len/Len_Migikata_no_chou.vmd;');
+        model2.setAttribute('mmd-model', 'model:images/len/len_2.pmx;vmd:images/len/Len_Migikata_no_chou.vmd;');
         model2.setAttribute('shadow', 'cast:true');
         music.append(model2);
 
@@ -52,7 +52,7 @@ AFRAME.registerComponent('vocaloid', {
         self.el.append(music);
 
         model = document.createElement("a-entity");
-        model.setAttribute('mmd-model', 'model:/images/luka/luka1.pmd;vmd:images/luka/night_fever.vmd;');
+        model.setAttribute('mmd-model', 'model:images/luka/luka1.pmd;vmd:images/luka/night_fever.vmd;');
         model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
@@ -66,7 +66,7 @@ AFRAME.registerComponent('vocaloid', {
         self.el.append(music);
 
         model = document.createElement("a-entity");
-        model.setAttribute('mmd-model', 'model:/images/meiko/meiko.pmx;vmd:images/meiko/nostalgic.vmd;');
+        model.setAttribute('mmd-model', 'model:images/meiko/meiko.pmx;vmd:images/meiko/nostalgic.vmd;');
         model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
@@ -80,17 +80,17 @@ AFRAME.registerComponent('vocaloid', {
         self.el.append(music);
 
         model = document.createElement("a-entity");
-        model.setAttribute('mmd-model', 'model:/images/kaito/hzeoV3kaito203.pmx; vmd:images/kaito/Tda_R.vmd;');
+        model.setAttribute('mmd-model', 'model:images/kaito/hzeoV3kaito203.pmx; vmd:images/kaito/Tda_R.vmd;');
         model.setAttribute('shadow', 'cast:true');
         music.append(model);
 
         model2 = document.createElement("a-entity");
-        model2.setAttribute('mmd-model', 'model:/images/len/len_2.pmx; vmd:images/kaito/Tda_C.vmd;');
+        model2.setAttribute('mmd-model', 'model:images/len/len_2.pmx; vmd:images/kaito/Tda_C.vmd;');
         model2.setAttribute('shadow', 'cast:true');
         music.append(model2);
 
         model3 = document.createElement("a-entity");
-        model3.setAttribute('mmd-model', 'model:/images/alt_miku/alt_miku.pmx; vmd:images/kaito/Tda_L.vmd;');
+        model3.setAttribute('mmd-model', 'model:images/alt_miku/alt_miku.pmx; vmd:images/kaito/Tda_L.vmd;');
         model3.setAttribute('shadow', 'cast:true');
         music.append(model3);
 
